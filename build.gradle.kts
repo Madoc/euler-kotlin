f0daa6bc3b3eaa27b0c52280e049e9c6cbb7b7a3
Matthias Deja <MadocDoyu@gmail.com>
2022-04-04 10:58:23 +0200

    Repo initialization.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+  kotlin("jvm") version "1.5.10"
+}
+
+group = "eu.madoc"
+version = "0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+}
