29a89672eb4256e5c4f7ad9c05bf21d03ee28afc
Matthias Deja <MadocDoyu@gmail.com>
2022-04-05 18:07:14 +0200

    Added a millisecond duration to each problem check.

--- src/main/kotlin/euler_kotlin/main.kt
+++ src/main/kotlin/euler_kotlin/main.kt
@@ -1,10 +1,20 @@
 package euler_kotlin
 
-fun main() {
-  for (problem in Problem.values()) {
-    print("Problem ${problem.number()}: ${problem.title} ")
-    val computedSolution: String = problem.implementation.computeSolution()
-    if (computedSolution == problem.solution.value) println("\u001B[32m✓\u001B[0m")
-    else println("\u001B[31m✗\u001B[0m Wrong computed solution ($computedSolution)")
-  }
+fun main(): Unit = Problem.values().forEach(::checkProblem)
+
+private fun checkProblem(problem: Problem) {
+  reportChecking(problem)
+  val startTimeMillis: Long = System.currentTimeMillis()
+  val computedSolution: String = problem.implementation.computeSolution()
+  val durationMillis: Long = System.currentTimeMillis() - startTimeMillis
+  if (computedSolution == problem.solution.value) reportSuccess(durationMillis)
+  else reportFailure(computedSolution, durationMillis)
 }
+
+private fun reportChecking(problem: Problem): Unit =
+  print("Problem ${problem.number()}: ${problem.title} ")
+
+private fun reportSuccess(durationMillis: Long): Unit = println("\u001B[32m✓\u001B[0m ($durationMillis ms)")
+
+private fun reportFailure(wrongSolution: String, durationMillis: Long): Unit =
+  println("\u001B[31m✗\u001B[0m ($durationMillis ms) Wrong computed solution ($wrongSolution)")
